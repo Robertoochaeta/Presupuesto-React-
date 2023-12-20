ba50d43c82e6be3e6c50f216a7fdf8d4d323d894
Roberto Ochaeta <mario24roberto@hotmail.com>
2023-12-20 11:13:59 -0600

    Creando el formulario principal

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NuevoPresupuestos from './NuevoPresupuesto'
+const Header = () => {
+  return (
+    <>
+    <header>
+        <h1>Planificador de Gastos</h1>
+        <NuevoPresupuestos />
+    </header>
+    </>
+  )
+}
+
+export default Header
